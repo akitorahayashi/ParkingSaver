9b649b54e5ff85e9c7dffa8dc7959d8a8bf9fd10
林明虎 <hayashi20030703@gmail.com>
2024-11-10 11:25:10 +0900

    Initial commit

---
+++ parking_saver/parking_saver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  parking_saver
+//
+//  Created by 林 明虎 on 2024/11/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ parking_saver/parking_saver/parking_saverApp.swift
@@ -0,0 +1,17 @@
+//
+//  parking_saverApp.swift
+//  parking_saver
+//
+//  Created by 林 明虎 on 2024/11/10.
+//
+
+import SwiftUI
+
+@main
+struct parking_saverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
